a114b5f0092da95973037668f491d54aef80cee7
mleya1 <141270959+mleya1@users.noreply.github.com>
2023-10-16 22:37:37 +0300

    Add ILJS site

---
+++ ILJS/Scripts/script.js
@@ -0,0 +1,18 @@
+function fb1() {
+    document.getElementById("ILJS").innerHTML = "ILJS is means I LIKE JAVASCRIPT, explore this site (this site made for fun :~))"
+}
+function fb2() {
+    document.getElementById("p2").innerHTML = "[i going say about runonce not person who working on ILJS  :~( ] runonce is have 12 UAH witch is less than 1 USD."
+}
+function relf() {
+    location.reload()
+}
+function email() {
+    window.location.href='mailto:aabobusfewdays@gmail.com'
+}
+function runoncehome() {
+    window.location.href='index.html'
+}
+function offline() {
+    
+}
